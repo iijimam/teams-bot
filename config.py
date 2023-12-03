48df013333a04fab5df3e52eee2d31bbe6b26d0c
iijimam <iijima@intersystems.com>
2023-12-03 09:52:10 +0900

    port変更

--- config.py
+++ config.py
@@ -10,7 +10,8 @@ import os
 class DefaultConfig:
     """ Bot Configuration """
 
-    PORT = 3978
+    #PORT = 3978
+    PORT = 8000
     APP_ID = os.environ.get("MicrosoftAppId", "80c19888-4244-4ec4-956e-b86a78c35907")
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "KMH8Q~Z2UMlxG8zMrMXtCpANg_yFoF3jgB-NbaWr")
     #APP_ID = os.environ.get("MicrosoftAppId", "")

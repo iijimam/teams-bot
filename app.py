6973c68ed344fbe99ef9df533d62c0546c79d863
iijimam <iijima@intersystems.com>
2023-12-03 10:34:46 +0900

    init_func追加

--- app.py
+++ app.py
@@ -91,6 +91,11 @@ async def messages(req: Request) -> Response:
 APP = web.Application(middlewares=[aiohttp_error_middleware])
 APP.router.add_post("/api/messages", messages)
 
+def init_func(argv):
+    APP = web.Application(middlewares=[aiohttp_error_middleware])
+    APP.router.add_post("/api/messages", messages)
+    return APP
+
 if __name__ == "__main__":
     try:
         #web.run_app(APP, host="localhost", port=CONFIG.PORT)
